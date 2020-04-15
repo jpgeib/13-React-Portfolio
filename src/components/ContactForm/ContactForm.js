4f55d7f434dd29504cd27758a44957c720f8d08e
James Geib <jpgeib2@gmail.com>
2020-04-15 13:20:10 -0700

    building prototype form for contact page

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,2 +1,28 @@
 import React from 'react';
 
+// import 'ContactForm.css';
+
+const ContactForm = props => {
+    return (
+        <div>
+            <form>
+                <h1>For business, please reach out to me by filling out the following form:</h1>
+                <div>
+                    <label className="name-label">Name:</label>
+                    <input id="name-bar" />
+                </div>
+                <div>
+                    <label className="email-label">Email:</label>
+                    <input id="email-bar" />
+                </div>
+                <div>
+                    <label className="descrip-label">Description:</label>
+                    <input id="descrip-bar" />
+                </div>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default ContactForm;
