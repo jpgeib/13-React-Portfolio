8cc1e5c8b5dc68a6f588022d079c7fe0c877a3a4
James Geib <jpgeib2@gmail.com>
2020-04-15 18:33:00 -0700

    working on rendering portrait

--- src/components/Portrait/Portrait.js
+++ src/components/Portrait/Portrait.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import portrait from '../../Images'
 import 'Portrait.css';
 
 const Portrait = props => {
     return (
         <div className="portrait-place">
-            <img className="img-fluid" id ="portrait" src="../../Images/Portfolio_Portrait.jpeg"/>
+            <img className="img-fluid" id ="portrait" src={portrait}/>
         </div>
     );
 }
