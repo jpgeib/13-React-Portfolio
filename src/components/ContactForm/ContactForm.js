d5dbdac60fa4f19eb7c6f3dda73bc8fd4752dc8d
James Geib <jpgeib2@gmail.com>
2020-04-17 21:21:06 -0700

    finished up the contact page, now just need to populate the portfolio page

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,25 +1,70 @@
 import React from 'react';
 
-// import 'ContactForm.css';
+import './ContactForm.css';
 
 const ContactForm = props => {
     return (
-        <div>
-            <form>
-                <h1>For business, please reach out to me by filling out the following form:</h1>
-                <div>
-                    <label className="name-label">Name:</label>
-                    <input id="name-bar" />
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-1">
+
                 </div>
-                <div>
-                    <label className="email-label">Email:</label>
-                    <input id="email-bar" />
+                <div className="col-sm-10">
+                    <h1 id="contact-heading">For business, please reach out to me by filling out the following form:</h1>
                 </div>
-                <div>
-                    <label className="descrip-label">Description:</label>
-                    <input id="descrip-bar" />
+                <div className="col-sm-1">
+
                 </div>
-                <button>Submit</button>
+            </div>
+            <br />
+            <form id="contact-form">
+                <div className="row">
+                    <div className="col-sm-3">
+
+                    </div>
+                    <div className="col-sm-6">
+                        <label className="name-label">Name:</label>
+                        <input id="name-bar" />
+                    </div>
+                </div>
+                <br />
+                <div className="row">
+                    <div className="col-sm-3">
+
+                    </div>
+                    <div className="col-sm-6">
+                        <label className="email-label">Email:</label>
+                        <input id="email-bar" />
+                    </div>
+                </div>
+                <br />
+                <div className="row">
+                    <div className="col-sm-3">
+
+                    </div>
+                    <div className="col-sm-6">
+                        <label className="descrip-label">Description:</label>
+                        <input id="descrip-bar" />
+                    </div>
+                </div>
+                <br />
+                <br />
+                <div className="row">
+                    <div className="col-sm-4">
+
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="row">
+                            <div className="col-sm-2">
+
+                            </div>
+                            <div className="col-sm-5">
+                                <button>Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </form>
         </div>
     );
--- src/containers/ContactContent/ContactContent.js
+++ src/containers/ContactContent/ContactContent.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+
 import Navbar from '../../components/Navbar/Navbar';
 import ContactForm from '../../components/ContactForm/ContactForm';
+import './ContactContent.css';
 
 class ContactContent extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <div className="container">
+                <div className="container" id="content-container">
                     <ContactForm/>
                 </div>
             </div>
